6ec93e49f97fa0c9ec242165443b66b85e4515c2
ShoroveTajmen <sorovitajmen@gmail.com>
2024-02-26 23:42:03 +0600

    initial set-up done

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="font-bold text-3xl">Shorove Tajmen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
