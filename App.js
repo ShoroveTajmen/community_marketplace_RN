1ae7a2b175ab7108d36179ad0a2a09673ce77c0c
ShoroveTajmen <sorovitajmen@gmail.com>
2024-02-27 21:42:20 +0600

    login authentication added

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import LoginScreen from "./Apps/Screens/LoginScreen";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white">
-      <Text className="font-bold text-3xl"></Text>
-      <StatusBar style="auto" />
-      <LoginScreen></LoginScreen>
-    </View>
+    <ClerkProvider publishableKey="pk_test_YW11c2luZy1ob3JzZS03OC5jbGVyay5hY2NvdW50cy5kZXYk">
+      <View className="flex-1 bg-white">
+        <Text className="font-bold text-3xl"></Text>
+        <StatusBar style="auto" />
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen></LoginScreen>
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,7 +1,30 @@
+import { useOAuth } from "@clerk/clerk-expo";
+import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const LoginScreen = () => {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View>
       <Image
@@ -14,7 +37,7 @@ const LoginScreen = () => {
           Buy Sell Marketplace where you can sell old item and make real money
         </Text>
         <TouchableOpacity
-          onPress={() => console.log("SignIn")}
+          onPress={onPress}
           className="p-4 bg-blue-500 rounded-full mt-20"
         >
           <Text className="text-white text-center text-[18px]">
