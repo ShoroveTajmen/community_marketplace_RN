57047d5a8f1704eb70b641ada9ff2a543d84eef6
ShoroveTajmen <sorovitajmen@gmail.com>
2024-02-28 12:17:13 +0600

    firebase added

---
+++ firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLIa7y1RKzsDepm7RETmqOgBDtGGtVL8k",
+  authDomain: "community-marketplace-95d73.firebaseapp.com",
+  projectId: "community-marketplace-95d73",
+  storageBucket: "community-marketplace-95d73.appspot.com",
+  messagingSenderId: "745712072783",
+  appId: "1:745712072783:web:3178634d446476faa5d23a",
+  measurementId: "G-5WJWWYMGS0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
