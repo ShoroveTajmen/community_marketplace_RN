86773bbfc1701e1c55f898581326311a2de6614d
ShoroveTajmen <sorovitajmen@gmail.com>
2024-02-28 14:02:37 +0600

    used get to fetch data from firebase

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { app } from "../../firebaseConfig";
 
 const AddPostScreen = () => {
+  const db = getFirestore(app);
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  // Used to get category list
+  const getCategoryList = async () => {
+    const querySnapshot = await getDocs(collection(db, "Category"));
+    querySnapshot.forEach((doc) => {
+      console.log("Docs:", doc.data());
+      setCategoryList((categoryList) => [...categoryList, doc.data()]);
+    });
+  };
+
   return (
     <View>
       <Text>Add Post Screen</Text>
